a79ca678a6645de53368daf5de09f9071058e3dc
Lucasc-99 <lucascecchi@gmail.com>
2022-04-23 20:53:50 -0400

    First commit done with gitc!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-use clap::Parser;
-
-#[derive(Parser)]
-struct CLi {
-    targets: Vec<String>,
-    commit_message: String
-}
+use std::process::Command;
 
 fn main() {
-    let args = CLi::parse();
-    println!("{:?}", args.targets); 
+    /*
+        A basic cli script that concatenates git add and git commit
+    */
+
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 3 {
+        panic!("Not enough arguments");
+    } else {
+        
+        // Parse args
+        let files = &args[1..args.len() - 1];
+        let commit_message = &args[args.len() - 1];
+
+        // Add 
+        let output = Command::new("git")
+        .arg("add")
+        .args(files)
+        .output()
+        .expect("failed to execute process");
+        
+        // Commit
+        let output = Command::new("git")
+            .arg("commit")
+            .arg("-m")
+            .arg(commit_message)
+            .output()
+            .expect("failed to execute process");
+    }
 }
