3258e0e174607aad3d76c8e4e6bbf42a420400f0
Lucasc-99 <lucascecchi@gmail.com>
2022-04-26 16:37:32 -0400

    add commit fn, and match statement in main for switch logic

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,48 @@
 use std::process::Command;
 
 /*
-    Fast Git (fgit):
+    Fast Git (fgi):
     A basic cli script that concatenates git add and git commit
 */
 
 fn main() {
-
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() < 3 {
-        panic!("Not enough arguments");
-    } else {
-        // Parse args
-        let files = &args[1..args.len() - 1];
-        let commit_message = &args[args.len() - 1];
+    // Parse Switch
+    let switch = &args[1];
 
-        // Add
-        let a_output = Command::new("git")
-            .arg("add")
-            .args(files)
-            .output()
-            .expect("failed to execute process");
+    match switch.as_str() {
+        "commit" => {
+            if args.len() < 4 {
+                println!("Usage: fgi commit files <message>");
+                return;
+            }
+            commit(&args[2..args.len() - 1], &args[args.len() - 1])
+        }
 
-        println!("{}", String::from_utf8_lossy(&a_output.stdout));
-
-        // Commit
-        let c_output = Command::new("git")
-            .arg("commit")
-            .arg("-m")
-            .arg(commit_message)
-            .output()
-            .expect("failed to execute process");
-
-        println!("{}", String::from_utf8_lossy(&c_output.stdout));
+        _ => {
+            println!("fgi: error: unknown switch");
+        }
     }
+}
+
+fn commit(files: &[String], commit_message: &String) {
+    // Add
+    let a_output = Command::new("git")
+        .arg("add")
+        .args(files)
+        .output()
+        .expect("failed to execute process");
+
+    println!("{}", String::from_utf8_lossy(&a_output.stdout));
+
+    // Commit
+    let c_output = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(commit_message)
+        .output()
+        .expect("failed to execute process");
+
+    println!("{}", String::from_utf8_lossy(&c_output.stdout));
 }
